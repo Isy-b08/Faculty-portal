66544ac67d6e1c7a924149eaa350bd1a62539c92
Isy-b08 <babalolaisrael2008@gmail.com>
2026-02-21 13:30:13 +0100

    Added landing page responsivenes

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const words = ["Students", "Engineers", "Programmers","Innovators"]
+const words = ["Students", "Engineers", "Developers","Innovators"]
 let index= 0;
 const wordElement=
   document.getElementById("changingword");
@@ -12,4 +12,14 @@ const wordElement=
    }, 200); 
   }, 2000);
 
-  
+  function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+  }
+  document.addEventListener('click', function(event) {
+    const nav = document.querySelector('nav');
+    const menuIcon = document.getElementById('menu-icon');  
+    if (!nav.contains(event.target) && !menuIcon.contains(event.target)) {
+      nav.classList.remove('active');
+    }
+  })
